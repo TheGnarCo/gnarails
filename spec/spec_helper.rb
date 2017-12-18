fde2c17feccaead474a9d7242085ee6d862d1ad9
Kevin Murphy <kevin@gnar.dog>
2017-12-17 21:27:38 -0500

    Remove requiring gnarly from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 require "bundler/setup"
 require "gnarails"
 require "rspec/expectations"
-# require_relative "../gnarly"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
