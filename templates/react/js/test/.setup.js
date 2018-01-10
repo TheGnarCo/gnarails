a322746eab52197796d92c1e83c065ed0a7a3f09
Mike Stone <mike@gnar.dog>
2018-01-10 09:42:22 -0500

    Update Enzyme adapter to React version 16

--- templates/react/js/test/.setup.js
+++ templates/react/js/test/.setup.js
@@ -1,7 +1,7 @@
 var jsdom = require('jsdom').JSDOM;
 var exposedProperties = ['window', 'navigator', 'document'];
 var Enzyme = require('enzyme');
-var Adapter = require('enzyme-adapter-react-15');
+var Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
 
