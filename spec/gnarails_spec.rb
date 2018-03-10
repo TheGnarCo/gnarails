693f3e7241775fb402f5711f29dfaa86708c8ab9
Kevin Murphy <kevin@gnar.dog>
2018-03-10 08:02:53 -0500

    dump temp bundler test

--- spec/gnarails_spec.rb
+++ spec/gnarails_spec.rb
@@ -4,11 +4,4 @@ RSpec.describe Gnarails do
   it "has a version number" do
     expect(Gnarails::VERSION).not_to be nil
   end
-
-  it "adds bundler-audit to the gemfile" do
-    gemfile = "rails-test-app/Gemfile"
-    contains_audit = File.readlines(gemfile).grep(/bundler-audit/).any?
-
-    expect(contains_audit).to be true
-  end
 end
