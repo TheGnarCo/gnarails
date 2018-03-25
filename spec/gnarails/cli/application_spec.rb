0e01356ea5a2f7afe58fcefca7ab58309d7a6824
Kevin Murphy <kevin@gnar.dog>
2018-03-24 23:15:48 -0400

    Support rails generator options
    This explicitly sets all of the different options from creating a new
    rails app that a gnarails application will accept. This includes all
    options that are available when generating a rails application, while
    excluding options that aren't applicable when using this template, such
    as the database option or the template option. These are explicitly set
    by using gnarails, and as such, are not available for users of this
    generator.

---
+++ spec/gnarails/cli/application_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+module Gnarails
+  module Cli
+    RSpec.describe Application do
+      describe "#new" do
+        it "creates a new rails application" do
+          application = Gnarails::Cli::Application.new
+
+          allow(Kernel).to receive(:system)
+
+          application.options = { webpack: "react", skip_yarn: false, skip_git: true }
+          application.new("name")
+
+          default_options = "-m #{Gnarails.template_file} --skip-test-unit --database=postgresql"
+          options = "#{default_options} --webpack=react --skip_git"
+
+          expect(Kernel).to have_received(:system)
+            .with("rails new name #{options}")
+        end
+      end
+    end
+  end
+end
