428418d26e576f6f961b6562170cf8db3cc30fe7
Reese Williams <rtwill722@gmail.com>
2019-08-22 13:53:38 -0400

    Remove chrome-driver helper from generated rails app for CI.

--- gnarly.rb
+++ gnarly.rb
@@ -84,6 +84,7 @@ def setup_testing
   setup_shoulda
   setup_bullet
   limit_test_logging
+  remove_chromedriver_helper
 end
 
 def setup_rspec
@@ -186,6 +187,10 @@ def limit_test_logging
   end
     TEST_LOGGING
   end
+end
+
+def remove_chromedriver_helper
+  gsub_file "Gemfile", /(\s+#.*\s+)gem 'chromedriver-helper'/, ""
 end
 
 def setup_analysis
